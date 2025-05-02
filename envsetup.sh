2300f9659d6dcdbb1585b996db6fec3ce8a2f514
jer-lil <35275270+jer-lil@users.noreply.github.com>
2025-05-02 11:56:28 -0700

    Added a ton of new parts and libraries for Tremolo V3

--- envsetup.sh
+++ envsetup.sh
@@ -1,11 +1,17 @@
 # define all of your libs here -- should be a CSV file for each lib
-GPLMLIBS="ana cap cpd dio ics ind mpu pot pwr rfm res reg xtr osc opt art"
+GPLMLIBS="cap res pot switch ics mcu amp ldo pwr bjt fet diode zener xtal tvs opto conn"
 
 parts_db_create() {
 	rm parts.sqlite
 
 	for lib in ${GPLMLIBS}; do
 		sqlite3 --csv ./parts.sqlite ".import ${lib}.csv ${lib}" || return 1
+	done
+}
+
+pull_libs() {
+	for lib in ${GPLMLIBS}; do
+		wget -O "${lib}.csv" "https://docs.google.com/spreadsheets/d/$1/gviz/tq?tqx=out:csv&sheet=${lib}"
 	done
 }
 
